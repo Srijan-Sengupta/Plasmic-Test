9027afd34c166920c60627657f4eb7cfc18b87e2
Srijan Sengupta <srijansengupta306@gmail.com>
2024-04-14 17:19:26 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './components/Homepage';
 
 function App() {
-  return (
+  /*return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,7 +20,11 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  );*/
+
+	return (
+		<Homepage/>
+	)
 }
 
 export default App;
